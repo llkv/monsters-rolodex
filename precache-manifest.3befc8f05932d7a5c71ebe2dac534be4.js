153aa506d6c58019d95ce93d8fe067fc6f4f5169
lvasina <ladallkv@yahoo.ca>
2020-03-02 16:26:11 -0500

    Updates

---
+++ precache-manifest.3befc8f05932d7a5c71ebe2dac534be4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f2c8e1fad35d6cc6a95f780319846b4",
+    "url": "/llkv/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c36744a8e601a88afb87",
+    "url": "/llkv/monsters-rolodex/static/css/main.7239ea69.chunk.css"
+  },
+  {
+    "revision": "f18ec448c3ab67b3b179",
+    "url": "/llkv/monsters-rolodex/static/js/2.9e4b70aa.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/llkv/monsters-rolodex/static/js/2.9e4b70aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c36744a8e601a88afb87",
+    "url": "/llkv/monsters-rolodex/static/js/main.d4146c61.chunk.js"
+  },
+  {
+    "revision": "08884a21e5f5306aa79c",
+    "url": "/llkv/monsters-rolodex/static/js/runtime-main.0da1b8ff.js"
+  }
+]);
